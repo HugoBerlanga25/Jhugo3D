93790a71210708ba4827912d63170a5084d620a4
ArnauSanchezZ <arnauzurano@gmail.com>
2026-01-26 09:10:03 +0100

    Add files via upload

---
+++ xarxes.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+
+<header>Xarxes Socials</header>
+
+<div class="icones">
+    <a href="https://www.tiktok.com/@_jhugo3d_"><img src="imatges/tiktok.png" alt="TikTok"></a>
+    <a href="https://www.instagram.com/jhugo3d"><img src="imatges/instagram.png" alt="Instagram"></a>
+</div>
+
+<?php include 'footer.php'; ?>
