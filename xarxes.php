ec5a8eeea1ba5d8304f6eba2f05cd11dfdcfb197
Hugo Berlanga <hugoberlangacalderon@gmail.com>
2026-01-26 10:25:16 +0100

    Update xarxes.php

--- xarxes.php
+++ xarxes.php
@@ -1,6 +1,7 @@
 <?php include 'header.php'; ?>
 
 <h1>Xarxes Socials</h1>
+<h2>Aqui podeu accedir a les meves xarxes socials, clica en la imatge per accedir!</h2>
 <div class="xarxes">
     <a href="https://www.tiktok.com/@_jhugo3d_"><img src="imatges/tiktok.png" alt="TikTok"></a>
     <a href="https://www.instagram.com/jhugo3d"><img src="imatges/instagram.png" alt="Instagram"></a>
