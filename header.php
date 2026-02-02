b997029aeb7c29cbbe5539b7a18a363f12896554
Hugo Berlanga <hugoberlangacalderon@gmail.com>
2026-02-02 10:06:07 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jhugo3D</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<header>
+    <nav>
+        <a href="index.php">Inicio</a>
+        <a href="disenys.php">Diseños</a>
+        <a href="xarxes.php">Redes Sociales</a>
+    </nav>
+</header>
+<main>
